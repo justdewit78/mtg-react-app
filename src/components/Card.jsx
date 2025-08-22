2bb399d499b93a3d4de76bb1b78998d65b73e37f
justdewit78 <31899252+justdewit78@users.noreply.github.com>
2025-08-22 15:17:23 -0500

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,16 @@
 // src/components/Card.jsx
 import React from 'react';
 
-export const Card = ({ card, onCardClick }) => {
+// Modified to accept both onClick and onDoubleClick props
+export const Card = ({ card, onClick, onDoubleClick }) => {
   const cardClasses = `card ${card.tapped ? 'tapped' : ''}`;
+
   return (
-    <div className={cardClasses} onClick={onCardClick}>
+    <div 
+      className={cardClasses} 
+      onClick={onClick} 
+      onDoubleClick={onDoubleClick}
+    >
       <div className="card-name">{card.name}</div>
       <div className="card-type">{card.type}</div>
     </div>
