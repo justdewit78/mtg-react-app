75b630b64772e0a71245345e38663281e602ff45
justdewit78 <31899252+justdewit78@users.noreply.github.com>
2025-08-22 15:42:00 -0500

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,28 +1,24 @@
-// src/components/Card.jsx
+/** @jsxRuntime classic */
+/** @jsx React.createElement */
 import React from 'react';
 
 export const Card = ({ card, onClick, onDoubleClick }) => {
   const cardClasses = `card ${card.tapped ? 'tapped' : ''}`;
 
   return (
-    <div 
-      className={cardClasses} 
-      onClick={onClick} 
+    <div
+      className={cardClasses}
+      onClick={onClick}
       onDoubleClick={onDoubleClick}
     >
       <div className="card-name">{card.name}</div>
       <div className="card-type">{card.type}</div>
 
-      {/* NEW: Tooltip for non-land cards */}
+      {/* NEW: Containers for cost and rules text - initially hidden */}
       {card.cost && (
-        <div className="card-tooltip">
-          <div className="tooltip-header">
-            <span>{card.name}</span>
-            <span>Cost: {card.cost.join('')}</span>
-          </div>
-          <div className="tooltip-body">
-            {card.rulesText ? card.rulesText : <em>(No rules text)</em>}
-          </div>
+        <div className="card-info">
+          <div className="card-cost">Cost: {card.cost.join('')}</div>
+          <div className="card-rules">{card.rulesText || <em></em>}</div>
         </div>
       )}
     </div>
